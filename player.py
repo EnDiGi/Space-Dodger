93c2850245d8e49a88a4db8791fe7b46333cf829
EnDiGi <degiorgienrico.26042012@gmail.com>
2024-08-15 15:42:24 +0200

    Update player.py

--- player.py
+++ player.py
@@ -5,14 +5,19 @@ class Player:
         self.y = scrHeight - height
         self.x = scrWidth // 2 - width // 2
         self.width = width
+        self.scrWidth = scrWidth
         
         self.sprite = pygame.Rect(self.x, self.y, width, height)
     
     def move(self, go_left):
-      
         if go_left:
             self.x -= 15
-        else:
+        elif not go_left:
             self.x += 15
+        
+        if self.x <= 0:
+            self.x = 0
+        elif self.x >= self.scrWidth - self.width:
+            self.x = self.scrWidth - self.width
         
         self.sprite.x = self.x
